ae15a184a0863dbd69ec48df12a32af74467331c
roncterry <15840212+roncterry@users.noreply.github.com>
2025-10-27 22:52:22 -0600

    fs.inotify.max_user_instances=1024

--- 01a-install_first_rke2_server.sh
+++ 01a-install_first_rke2_server.sh
@@ -60,6 +60,10 @@ check_for_helm() {
 }
 
 install_k8s_distro() {
+  echo "Setting sysctl fs.inotify.max_user_instances=1024"
+  echo "fs.inotify.max_user_instances=1024" > /etc/sysctl.d/50-fs_inotify_max_user_instances.conf
+  sysctl fs.inotify.max_user_instances=1024
+
   echo "Downloading ${K8S_DISTRO} installer ..."
   echo "COMMAND: curl -sfL https://get.${K8S_DISTRO}.io --output /root/${K8S_DISTRO}-install.sh"
   curl -sfL https://get.${K8S_DISTRO}.io --output /root/${K8S_DISTRO}-install.sh
