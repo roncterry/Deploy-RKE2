803a7faf70e8201c82b66a93ca9859034d60c925
roncterry <15840212+roncterry@users.noreply.github.com>
2026-02-10 23:59:47 -0700

    source config and rm symlink

--- 02c-symlink_kubectl.sh
+++ 02c-symlink_kubectl.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+CONFIG_FILE=deploy_rke2.cfg
+
+if ! [ -z ${CONFIG_FILE} ]
+then
+  if [ -e ${CONFIG_FILE} ]
+  then
+    source ${CONFIG_FILE}
+  fi
+else
+  K8S_DISTRO=rke2
+  CLUSTER_NAME=c01
+  DOMAIN_NAME=example.com
+fi
+
+if [ -s /usr/local/bin/kubectl ]
+then
+  rm /usr/local/bin/kubectl
+fi
+
 echo
 echo "COMMAND: ln -s /var/lib/rancher/${K8S_DISTRO}/bin/kubectl /usr/local/bin/"
 ln -s /var/lib/rancher/${K8S_DISTRO}/bin/kubectl /usr/local/bin/
