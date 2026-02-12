fb24ef1b05d024b9a74c692f15f025c52264d886
roncterry <15840212+roncterry@users.noreply.github.com>
2026-02-11 21:52:43 -0700

    Initial commit

---
+++ suse_ai/62-install_milvus.sh
@@ -0,0 +1,299 @@
+#!/bin/bash
+
+##############################################################################
+
+# You can either source in the variables from a common config file or
+# set the them in this script.
+
+CONFIG_FILE=deploy_suse_ai.cfg
+
+if ! [ -z ${CONFIG_FILE} ]
+then
+  if [ -e ${CONFIG_FILE} ]
+  then
+    source ${CONFIG_FILE}
+  fi
+else
+  #### General Variables  #####
+  SUSE_AI_NAMESPACE=suse-ai
+  IMAGE_PULL_SECRET_NAME=application-collection
+  STORAGE_CLASS_NAME=longhorn
+  MILVUS_VERSION=
+  MILVUS_CLUSTER_ENABLED=false
+  MILVUS_LOGGING_LEVEL=info
+  MILVUS_LOGGING_STORAGE=emptyDir
+  
+  MILVUS_STANDALONE_MESSAGE_QUEUE=rocksmq
+
+  ####  Etcd Variables  #####
+  MILVUS_ETCD_ENABLED=true
+  MILVUS_ETCD_REPLICA_COUNT=1
+
+  ####  Mineo Vairables  #####
+  MILVUS_MINIO_ENABLED=true
+  MILVUS_MINIO_ROOT_USER=admin
+  MILVUS_MINIO_ROOT_USER_PASSWORD=adminminio
+  MILVUS_MINIO_MODE=standalone
+  MILVUS_MINIO_REPLICA_COUNT=1
+  MILVUS_MINIO_VOLUME_SIZE=100Gi
+  MILVUS_MINIO_MEMORY=4096Mi 
+  
+  ####  Kafka Vairables  #####
+  MILVUS_KAFKA_ENABLED=false
+  MILVUS_KAFKA_REPLICA_COUNT=3
+  MILVUS_KAFKA_VOLUME_SIZE=8Gi
+fi
+
+LICENSES_FILE=../authentication_and_licenses.cfg
+
+CUSTOM_OVERRIDES_FILE=milvus_custom_overrides.yaml
+
+##############################################################################
+
+check_for_kubectl() {
+  if ! echo $* | grep -q force
+  then
+   if ! which kubectl > /dev/null
+   then
+     echo
+     echo "ERROR: This must be run on a machine with the kubectl command installed."
+     echo "       Run this script on a control plane node or management machine."
+     echo
+     echo "       Exiting."
+     echo
+     exit
+   fi
+  fi
+}
+
+check_for_helm() {
+  if ! echo $* | grep -q force
+  then
+   if ! which helm > /dev/null
+   then
+     echo
+     echo "ERROR: This must be run on a machine with the helm command installed."
+     echo "       Run this script on a control plane node or management machine."
+     echo
+     echo "       Exiting."
+     echo
+     exit
+   fi
+  fi
+}
+
+##############################################################################
+
+log_into_app_collection() {
+  if [ -z ${APP_COLLECTION_USERNAME} ]
+  then
+    # The APP_COLLECTION_URI, APP_COLLECTION_USERNAME and APP_COLLECTION_PASSWORD
+    # variables are set in an external file and are sourced in here:
+    source ${LICENSES_FILE}
+  fi
+
+  echo "Logging into the Application Collection ..."
+  echo "COMMAND: helm registry login dp.apps.rancher.io/charts -u ${APP_COLLECTION_USERNAME} -p ${APP_COLLECTION_PASSWORD}"
+  helm registry login dp.apps.rancher.io/charts -u ${APP_COLLECTION_USERNAME} -p ${APP_COLLECTION_PASSWORD}
+  echo
+}
+
+create_milvus_custom_overrides_file() {
+  echo "Writing out ${CUSTOM_OVERRIDES_FILE} file ..."
+  echo
+  echo "
+global:
+  imagePullSecrets:
+  - ${IMAGE_PULL_SECRET_NAME}
+cluster:
+  enabled: ${MILVUS_CLUSTER_ENABLED}
+log:
+  level: ${MILVUS_LOGGING_LEVEL}" > ${CUSTOM_OVERRIDES_FILE}
+
+  #####  logging storage values  #####
+  case ${MILVUS_LOGGING_STORAGE} in
+    storageClass)
+      echo "  persistence:
+    enabled: true
+    persistentVolumeClaim:
+      storageClass: ${STORAGE_CLASS_NAME}" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+
+  #####  standalone mode values  #####
+  echo "standalone:
+  messageQueue: ${MILVUS_STANDALONE_MESSAGE_QUEUE}
+  persistence:
+    persistentVolumeClaim:
+      storageClass: ${STORAGE_CLASS_NAME}" >> ${CUSTOM_OVERRIDES_FILE}
+
+  #####  ectd values  #####
+  case ${MILVUS_ETCD_ENABLED} in
+    true)
+      echo "etcd:
+  enabled: ${MILVUS_ETCD_ENABLED}
+  replicaCount: ${MILVUS_ETCD_REPLICA_COUNT}
+  persistence:
+    storageClassName: ${STORAGE_CLASS_NAME}" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+    false)
+      echo "etcd:
+  enabled: ${ETCD_ENABLED}" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+
+  #####  MinIO values  #####
+  case ${MILVUS_MINIO_ENABLED} in
+    true)
+      echo "minio:
+  enabled: ${MILVUS_MINIO_ENABLED}
+  mode: ${MILVUS_MINIO_MODE}
+  replicas: ${MILVUS_MINIO_REPLICA_COUNT}
+  rootUser: ${MILVUS_MINIO_ROOT_USER}
+  rootPassword: ${MILVUS_MINIO_ROOT_USER_PASSWORD}
+  persistence:
+    size: ${MILVUS_MINIO_VOLUME_SIZE}
+    storageClass: ${STORAGE_CLASS_NAME}
+  resources:
+    requests:
+      memory: ${MILVUS_MINIO_MEMORY}" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+    false)
+      echo "minio:
+  enabled: ${MILVUS_MINIO_ENABLED}" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+
+  #####  Kafka values  #####
+  if [ -z ${MILVUS_KAFKA_STORAGE_CLASS_NAME} ]
+  then
+    MILVUS_KAFKA_STORAGE_CLASS_NAME=${STORAGE_CLASS_NAME}
+  fi
+
+  case ${MILVUS_KAFKA_ENABLED} in
+    true)
+      echo "kafka:
+  enabled: ${MILVUS_KAFKA_ENABLED}
+  name: kafka
+  replicaCount: ${MILVUS_KAFKA_REPLICA_COUNT}
+  broker:
+    enabled: true
+  cluster:
+    listeners:
+      client:
+        protocol: 'PLAINTEXT'
+      controller:
+        protocol: 'PLAINTEXT'
+  persistence:
+    enabled: true
+    annotations: {}
+    labels: {}
+    existingClaim: \"\"
+    accessModes:
+    - ReadWriteOnce
+    resources:
+      requests:
+        storage: ${MILVUS_KAFKA_VOLUME_SIZE}
+    storageClassName: ${MILVUS_KAFKA_STORAGE_CLASS_NAME}" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+    false)
+      echo "kafka:
+  enabled: ${MILVUS_KAFKA_ENABLED} " >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+
+  case ${MILVUS_TELEMETRY_ENABLED} in
+    true|True)
+      echo "extraConfigFiles:
+  user.yaml: |+
+    trace:
+      exporter: jaeger
+      sampleFraction: 1
+      jaeger:
+        url: \"http://opentelemetry-collector.${OTEL_NAMESPACE}.svc.cluster.local:14268/api/traces\"" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+  echo
+}
+
+display_custom_overrides_file() {
+  echo
+  cat ${CUSTOM_OVERRIDES_FILE}
+  echo
+}
+
+deploy_milvus() {
+  if ! [ -z ${MILVUS_VERSION} ]
+  then
+    local MILVUS_VER_ARG="--version ${MILVUS_VERSION}"
+  fi
+
+  echo "COMMAND: helm upgrade --install milvus \
+    -n ${SUSE_AI_NAMESPACE} --create-namespace \
+    -f ${CUSTOM_OVERRIDES_FILE} \
+    oci://dp.apps.rancher.io/charts/milvus ${MILVUS_VER_ARG}"
+
+  helm upgrade --install milvus \
+    -n ${SUSE_AI_NAMESPACE} --create-namespace \
+    -f ${CUSTOM_OVERRIDES_FILE} \
+    oci://dp.apps.rancher.io/charts/milvus ${MILVUS_VER_ARG}
+
+  case ${MILVUS_CLUSTER_ENABLED} in
+    false|False|FALSE)
+      echo
+      echo "COMMAND: kubectl -n ${SUSE_AI_NAMESPACE} rollout status deploy/milvus-minio"
+      kubectl -n ${SUSE_AI_NAMESPACE} rollout status deploy/milvus-minio
+      echo
+      echo "COMMAND: kubectl -n ${SUSE_AI_NAMESPACE} rollout status deploy/milvus-standalone"
+      kubectl -n ${SUSE_AI_NAMESPACE} rollout status deploy/milvus-standalone
+    ;;
+  esac
+
+  echo
+}
+
+usage() {
+  echo
+  echo "USAGE: ${0} [custom_overrides_only|install_only]"
+  echo
+  echo "Options: "
+  echo "    custom_overrides_only  (only write out the ${CUSTOM_OVERRIDES_FILE} file)"
+  echo "    install_only           (only run an install using an existing ${CUSTOM_OVERRIDES_FILE} file)"
+  echo
+  echo "If no option is supplied the ${CUSTOM_OVERRIDES_FILE} file is created and"
+  echo "is used to perform an installation using 'helm upgrade --install'."
+  echo
+  echo "Example: ${0}"
+  echo "         ${0} custom_overrides_only"
+  echo "         ${0} install_only"
+  echo
+}
+
+##############################################################################
+
+case ${1} in
+  custom_overrides_only)
+    create_milvus_custom_overrides_file
+    display_custom_overrides_file
+  ;;
+  install_only)
+    check_for_kubectl
+    check_for_helm
+    log_into_app_collection
+    display_custom_overrides_file
+    deploy_milvus
+  ;;
+  help|-h|--help)
+    usage
+    exit
+  ;;
+  *)
+    check_for_kubectl
+    check_for_helm
+    log_into_app_collection
+    create_milvus_custom_overrides_file
+    display_custom_overrides_file
+    deploy_milvus
+  ;;
+esac
+
---
+++ suse_ai/65-install_open-webui_with_ollama.sh
@@ -0,0 +1,410 @@
+#!/bin/bash
+
+##############################################################################
+
+# You can either source in the variables from a common config file or
+# set the them in this script.
+
+CONFIG_FILE=deploy_suse_ai.cfg
+
+if ! [ -z ${CONFIG_FILE} ]
+then
+  if [ -e ${CONFIG_FILE} ]
+  then
+    source ${CONFIG_FILE}
+  fi
+else
+  SUSE_AI_NAMESPACE=suse-ai
+  IMAGE_PULL_SECRET_NAME=application-collection
+  STORAGE_CLASS_NAME=longhorn
+
+  OWUI_OLLAMA_ENABLED=true
+  OWUI_INGRESS_HOST=webui.example.com
+  OLLAMA_MODEL_0=llama3.2
+  OLLAMA_MODEL_1=gemma:2b
+  OLLAMA_MODEL_2=
+  OLLAMA_MODEL_3=
+  OLLAMA_MODEL_4=
+fi
+
+LICENSES_FILE=../authentication_and_licenses.cfg
+
+CUSTOM_OVERRIDES_FILE=owui_custom_overrides.yaml
+
+##############################################################################
+
+check_for_kubectl() {
+  if ! echo $* | grep -q force
+  then
+   if ! which kubectl > /dev/null
+   then
+     echo
+     echo "ERROR: This must be run on a machine with the kubectl command installed."
+     echo "       Run this script on a control plane node or management machine."
+     echo
+     echo "       Exiting."
+     echo
+     exit
+   fi
+  fi
+}
+
+check_for_helm() {
+  if ! echo $* | grep -q force
+  then
+   if ! which helm > /dev/null
+   then
+     echo
+     echo "ERROR: This must be run on a machine with the helm command installed."
+     echo "       Run this script on a control plane node or management machine."
+     echo
+     echo "       Exiting."
+     echo
+     exit
+   fi
+  fi
+}
+
+##############################################################################
+
+log_into_app_collection() {
+  if [ -z ${APP_COLLECTION_USERNAME} ]
+  then
+    # The APP_COLLECTION_URI, APP_COLLECTION_USERNAME and APP_COLLECTION_PASSWORD
+    # variables are set in an external file and are sourced in here:
+    source ${LICENSES_FILE}
+  fi
+
+
+  echo "Logging into the Application Collection ..."
+  echo "COMMAND: helm registry login dp.apps.rancher.io/charts -u ${APP_COLLECTION_USERNAME} -p ${APP_COLLECTION_PASSWORD}"
+  helm registry login dp.apps.rancher.io/charts -u ${APP_COLLECTION_USERNAME} -p ${APP_COLLECTION_PASSWORD}
+  echo
+}
+
+#install_certmanager_crds() {
+#  echo
+#  echo "COMMAND: kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.6.2/cert-manager.crds.yaml"
+#  kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.6.2/cert-manager.crds.yaml
+#  echo
+#}
+
+create_owui_base_custom_overrides_file() {
+  echo "Writing out ${CUSTOM_OVERRIDES_FILE} file ..."
+  echo
+  echo "
+global:
+  imagePullSecrets:
+  - ${IMAGE_PULL_SECRET_NAME}" > ${CUSTOM_OVERRIDES_FILE}
+
+  ######  TLS values  ######
+  echo "  tls:
+    source: ${OWUI_TLS_SOURCE}" >> ${CUSTOM_OVERRIDES_FILE}
+  case ${OWUI_TLS_SOURCE} in
+    letsEncrypt)
+      echo "    letsEncrypt:
+      email: ${OWUI_TLS_EMAIL}
+      environment: ${OWUI_TLS_LETSENCRYPT_ENVIRONMENT}
+      ingress:
+        class: \"${OWUI_TLS_INGRESS_CLASS}\"" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+    secret)
+      echo "    additionalTrustedCerts: ${OWUI_TLS_ADDITIONAL_TRUSTED_CERTS}" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+    *)
+      echo "    additionalTrustedCerts: ${OWUI_TLS_ADDITIONAL_TRUSTED_CERTS}" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+
+  ######  persistent storage values  ######
+  echo "persistence:
+  enabled: true
+  storageClass: ${STORAGE_CLASS_NAME}
+ingress:
+  class: nginx
+  host: ${OWUI_INGRESS_HOST}" >> ${CUSTOM_OVERRIDES_FILE}
+}
+
+add_ollama_config_to_custom_overrides_file() {
+  echo "ollama:
+  enabled: ${OWUI_OLLAMA_ENABLED}" >> ${CUSTOM_OVERRIDES_FILE}
+
+  case ${OWUI_OLLAMA_ENABLED} in
+    True|true|TRUE)
+      echo "  ollama:
+    models:
+      pull:" >> ${CUSTOM_OVERRIDES_FILE}
+
+      if ! [ -z ${OLLAMA_MODEL_0} ]
+      then
+        echo "      - \"${OLLAMA_MODEL_0}\" " >> ${CUSTOM_OVERRIDES_FILE}
+      fi
+  
+      if ! [ -z ${OLLAMA_MODEL_1} ]
+      then
+        echo "      - \"${OLLAMA_MODEL_1}\" " >> ${CUSTOM_OVERRIDES_FILE}
+      fi
+  
+      if ! [ -z ${OLLAMA_MODEL_2} ]
+      then
+        echo "      - \"${OLLAMA_MODEL_2}\" " >> ${CUSTOM_OVERRIDES_FILE}
+      fi
+  
+      if ! [ -z ${OLLAMA_MODEL_3} ]
+      then
+        echo "      - \"${OLLAMA_MODEL_3}\" " >> ${CUSTOM_OVERRIDES_FILE}
+      fi
+  
+      if ! [ -z ${OLLAMA_MODEL_4} ]
+      then
+        echo "      - \"${OLLAMA_MODEL_4}\" " >> ${CUSTOM_OVERRIDES_FILE}
+      fi
+    ;;
+    False|false|FALSE)
+      echo "ollamaURLSs:
+  - http://ollama.${SUSE_AI_NAMESPACE}.svc.cluster.local:11434" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+}
+
+add_nvidia_gpu_to_custom_overrides_file() {
+  case ${OWUI_OLLAMA_ENABLED} in
+    True|true|TRUE)
+      echo "    gpu:
+      enabled: ${OLLAMA_GPU_ENABLED}
+      type: ${OLLAMA_GPU_TYPE}
+      number: ${OLLAMA_GPU_NUMBER}
+      nvidiaResource: ${OLLAMA_GPU_NVIDIA_RESOURCE}
+    runtimeClassName: nvidia" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+}
+
+add_pipelines_to_custom_overrides_file() {
+  case ${OWUI_PIPELINES_ENABLED} in
+    True|true|TRUE)
+      echo "pipelines:
+  enabled: true
+  persistence:
+    storageClass: ${STORAGE_CLASS_NAME}
+  extraEnvVars:
+    - name: PIPELINES_URLS
+      value: \"${OWUI_PIPELINES_URLS}\"
+    - name: OTEL_SERVICE_NAME
+      value: \"${OWUI_PIPELINES_OTEL_SERVICE_NAME}\"
+    - name: OTEL_EXPORTER_HTTP_OTLP_ENDPOINT
+      value: \"${OWUI_PIPELINES_OTEL_EXPORTER_HTTP_OTLP_ENDPOINT}\"
+    - name: PRICING_JSON
+      value: \"${OWUI_PIPELINES_PRICING_JSON}\"" >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+}
+
+add_extra_envvars_to_custom_overrides_file() {
+  echo "extraEnvVars:" >> ${CUSTOM_OVERRIDES_FILE}
+
+  echo "- name: DEFAULT_MODELS
+  value: \"${OLLAMA_MODEL_0}\"
+- name: DEFAULT_USER_ROLE
+  value: \"pending\"
+- name: ENABLE_SIGNUP
+  value: \"true\"
+- name: WEBUI_NAME
+  value: \"SUSE AI\"" >> ${CUSTOM_OVERRIDES_FILE}
+#- name: GLOBAL_LOG_LEVEL
+#  value: \"INFO\"" >> ${CUSTOM_OVERRIDES_FILE}
+}
+
+add_milvus_extra_envvars_to_custom_overrides_file() {
+  echo "- name: VECTOR_DB
+  value: \"milvus\"
+- name: MILVUS_URI
+  value: \"http://milvus.${SUSE_AI_NAMESPACE}.svc.cluster.local:19530\"
+- name: RAG_EMBEDDING_MODEL
+  value: \"sentence-transformers/all-MiniLM-L6-v2\"" >> ${CUSTOM_OVERRIDES_FILE}
+#    - name: INSTALL_NLTK_DATASETS
+#      value: \"true\"" >> ${CUSTOM_OVERRIDES_FILE}
+}
+
+add_otel_extra_envvars_to_custom_overrides_file() {
+  echo "- name: ENABLE_OTEL
+  value: \"true\"
+- name: OTEL_EXPORTER_OTLP_ENDPOINT
+  value: http://opentelemetry-collector.${OTEL_NAMESPACE}.svc.cluster.local:4317 " >> ${CUSTOM_OVERRIDES_FILE}
+}
+
+add_pipelines_extra_envvars_to_custom_overrides_file() {
+  case ${OWUI_PIPELINES_ENABLED} in
+    true)
+      echo "- name: OPENAI_API_KEY
+  value: \"0p3n-w3bu!\" " >> ${CUSTOM_OVERRIDES_FILE}
+    ;;
+  esac
+}
+
+display_custom_overrides_file() {
+  echo
+  cat ${CUSTOM_OVERRIDES_FILE}
+  echo
+}
+
+install_open_webui() {
+  if ! [ -z ${OWUI_VERSION} ]
+  then
+    local OWUI_VER_ARG="--version ${OWUI_VERSION}"
+  fi
+
+  echo
+  echo "COMMAND:
+  helm upgrade --install open-webui \
+    -n ${SUSE_AI_NAMESPACE} --create-namespace \
+    -f ${CUSTOM_OVERRIDES_FILE} \
+    oci://dp.apps.rancher.io/charts/open-webui ${OWUI_VER_ARG}"
+
+  helm upgrade --install open-webui \
+    -n ${SUSE_AI_NAMESPACE} --create-namespace \
+    -f ${CUSTOM_OVERRIDES_FILE} \
+    oci://dp.apps.rancher.io/charts/open-webui ${OWUI_VER_ARG}
+
+  case ${OWUI_OLLAMA_ENABLED} in
+    true|True|TRUE)
+      echo
+      echo "COMMAND: kubectl -n ${SUSE_AI_NAMESPACE} rollout status deploy/open-webui-ollama"
+      kubectl -n ${SUSE_AI_NAMESPACE} rollout status deploy/open-webui-ollama
+    ;;
+  esac
+
+  echo
+}
+
+usage() {
+  echo
+  echo "USAGE: ${0} <option>"
+  echo
+  echo " You must supply one of the following options: "
+  echo "   without_gpu                      (install without GPU support enabled)"
+  echo "   with_gpu                         (install with GPU support enabled)"
+  echo "   with_gpu_and_milvus              (install with GPU support and Milvus enabled)"
+  echo "   with_external_ollama_and_milvus  (install using external Ollama and Milvus enabled)"
+  echo "   custom_overrides_only            (only write out the ${CUSTOM_OVERRIDES_FILE} file)"
+  echo "   install_only                     (only run an install using an existing ${CUSTOM_OVERRIDES_FILE} file)"
+  echo
+  echo "Example: ${0} without_gpu"
+  echo "         ${0} with_gpu"
+  echo "         ${0} with_gpu_and_milvus"
+  echo "         ${0} with_external_ollama_and_milvus"
+  echo "         ${0} custom_overrides_only"
+  echo "         ${0} install_only"
+  echo
+}
+
+##############################################################################
+
+
+case ${1} in
+  custom_overrides_only)
+    create_owui_base_custom_overrides_file
+    add_ollama_config_to_custom_overrides_file
+    add_nvidia_gpu_to_custom_overrides_file
+    add_pipelines_to_custom_overrides_file
+
+    # add extra envvars
+    add_extra_envvars_to_custom_overrides_file
+    add_milvus_extra_envvars_to_custom_overrides_file
+    add_otel_extra_envvars_to_custom_overrides_file
+    add_pipelines_extra_envvars_to_custom_overrides_file
+
+    # display values file
+    display_custom_overrides_file
+  ;;
+  install_only)
+    check_for_kubectl
+    check_for_helm
+    log_into_app_collection
+    display_custom_overrides_file
+
+    # install
+    install_open_webui
+  ;;
+  without_gpu)
+    check_for_kubectl
+    check_for_helm
+    log_into_app_collection
+    create_owui_base_custom_overrides_file
+    add_ollama_config_to_custom_overrides_file
+    add_pipelines_to_custom_overrides_file
+
+    # add extra envvars
+    add_extra_envvars_to_custom_overrides_file
+    add_otel_extra_envvars_to_custom_overrides_file
+
+    # display values file
+    display_custom_overrides_file
+
+    # install
+    install_open_webui
+  ;;
+  with_gpu)
+    check_for_kubectl
+    check_for_helm
+    log_into_app_collection
+    create_owui_base_custom_overrides_file
+    add_ollama_config_to_custom_overrides_file
+    add_nvidia_gpu_to_custom_overrides_file
+    add_pipelines_to_custom_overrides_file
+
+    # add extra envvars
+    add_extra_envvars_to_custom_overrides_file
+
+    # display values file
+    display_custom_overrides_file
+
+    # install
+    install_open_webui
+  ;;
+  with_gpu_and_milvus)
+    check_for_kubectl
+    check_for_helm
+    log_into_app_collection
+    create_owui_base_custom_overrides_file
+    add_ollama_config_to_custom_overrides_file
+    add_nvidia_gpu_to_custom_overrides_file
+    add_pipelines_to_custom_overrides_file
+
+    # add extra envvars
+    add_extra_envvars_to_custom_overrides_file
+    add_milvus_extra_envvars_to_custom_overrides_file
+    add_otel_extra_envvars_to_custom_overrides_file
+    add_pipelines_extra_envvars_to_custom_overrides_file
+
+    # display values file
+    display_custom_overrides_file
+
+    # install
+    install_open_webui
+  ;;
+  with_external_ollama_and_milvus)
+    check_for_kubectl
+    check_for_helm
+    log_into_app_collection
+    create_owui_base_custom_overrides_file
+    add_ollama_config_to_custom_overrides_file
+    add_pipelines_to_custom_overrides_file
+
+    # add extra envvars
+    add_extra_envvars_to_custom_overrides_file
+    add_milvus_to_custom_overrides_file
+    add_otel_to_custom_overrides_file
+
+    # display values file
+    display_custom_overrides_file
+
+    # install
+    install_open_webui
+  ;;
+  *)
+    usage
+    exit
+  ;;
+esac
+
